74168954408b43ca1dff1f903c1516b4d4f7b80e
KovalenkoVlad <kovalenkovlad420@knu.ua>
2023-05-25 10:58:12 +0300

    Make migrations

---
+++ recipe/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category,Recipe
+
+def main(request):
+    recipes = Recipe.objects.filter(year=2023)
+    return render(request, 'main.html', {'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
+
